86967b0d52047460198322a253b8dfa7bd7ed4fa
icodening <747167846@qq.com>
2021-05-13 23:12:38 +0800

    合并两个有序链表

--- simple/src/cn/icodening/leetcode/s21/MergeTwoLists.java
+++ simple/src/cn/icodening/leetcode/s21/MergeTwoLists.java
@@ -1,8 +1,105 @@
 package cn.icodening.leetcode.s21;
 
 /**
+ * 21. 合并两个有序链表
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author icodening
  * @date 2021.05.13
  */
 public class MergeTwoLists {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode listNode = mergeTwoLists(listNode1, listNode2);
+        System.out.println(listNode);
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+
+//
+//        ListNode prehead = new ListNode(-1);
+//
+//        ListNode prev = prehead;
+//        while (l1 != null && l2 != null) {
+//            if (l1.val <= l2.val) {
+//                prev.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                prev.next = l2;
+//                l2 = l2.next;
+//            }
+//            prev = prev.next;
+//        }
+//
+//        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+//        prev.next = l1 == null ? l2 : l1;
+//
+//        return prehead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
 }
+
+
